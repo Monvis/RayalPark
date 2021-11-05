3a07d785b513c18ea7b3339645f2481d462cb3fb
Hatvis7 <shamil.gasanov.2004@bk.ru>
2021-11-05 21:55:55 +0300

    Добавлены якори, правки по адаптивности

--- dist/source/js/script.js
+++ dist/source/js/script.js
@@ -38,7 +38,33 @@ $(function(){
   body.classList.toggle('lock-scroll');
   burger.classList.toggle('burger__active');
   });
+
+  //video
+  const video1 = document.querySelector('.video');
+
+  function videoPausePlayHandler(e) {
+    if (e.type == 'playing') {
+      video1.setAttribute("controls","controls");
+    }
+  }
+
+  video1.addEventListener('playing', videoPausePlayHandler, false);
+
+  //anchors
+  let anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (anchor of anchors) {
+    if (anchor) {
+      anchor.addEventListener('click', function(e){
+          e.preventDefault();
+          anchorID = this.getAttribute('href');
+          console.log(anchorID);
+          document.querySelector(anchorID).scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          })
+      })
+    }
+  }
+
 })
-
-// Learning
-
